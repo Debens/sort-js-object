cfa6f5a0a1b5f1adc01738daa852af06c1004ea7
Debens <a.debens@gmail.com>
2019-09-16 11:53:33 +0100

    feat: adding a command to format an entire file

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,26 @@
 import { sortText } from './sort';
-import { getSelectedText, setSelectedText } from './utils/text';
+import { getSelectedText, getFileText, setFileText, setSelectedText } from './utils/text';
 import * as vscode from 'vscode';
 import { noop } from './utils/functions';
 import { getOptions } from './utils/options';
 
 import * as R from 'ramda';
-
-export const activate = (context: vscode.ExtensionContext) => {
-    const disposable = vscode.commands.registerCommand(
-        'sort-js-object.sort',
-        R.pipe(
-            R.converge(sortText, [getSelectedText, getOptions]),
-            setSelectedText,
+export const activate = (context: vscode.ExtensionContext) =>
+    context.subscriptions.concat([
+        vscode.commands.registerCommand(
+            'sort-js-object.sort',
+            R.pipe(
+                R.converge(sortText, [getSelectedText, getOptions]),
+                setSelectedText,
+            ),
         ),
-    );
-
-    context.subscriptions.push(disposable);
-};
+        vscode.commands.registerCommand(
+            'sort-js-object.sort-file',
+            R.pipe(
+                R.converge(sortText, [getFileText, getOptions]),
+                setFileText,
+            ),
+        ),
+    ]);
 
 export const deactivate = noop;
--- src/utils/text.ts
+++ src/utils/text.ts
@@ -6,10 +6,24 @@ export const getSelectedText = () => {
     return editor.document.getText(editor.selection);
 };
 
-export const setSelectedText = (text: string) => {
+export const getFileText = () => {
     const editor = getEditor();
 
-    return editor.edit(builder => builder.replace(editor.selection, text));
+    return editor.document.getText();
+};
+
+export const setSelectedText = (text: string) => setText(getEditor().selection, text);
+
+export const setFileText = (text: string) =>
+    setText(documentRange(getEditor().document), text);
+
+const setText = (range: vscode.Range, text: string) =>
+    getEditor().edit(builder => builder.replace(range, text));
+
+const documentRange = (document: vscode.TextDocument): vscode.Range => {
+    const last = document.lineCount - 1;
+
+    return new vscode.Range(0, 0, last, document.lineAt(last).text.length);
 };
 
 export const getEditor = () => vscode.window.activeTextEditor;
